cfd5141150078c2e97e604e81ad166cd7ebfbd37
MZdrenka <mar.zdr99@gmail.com>
2025-05-15 11:58:08 +0200

    no message

---
+++ Assets/[Script]/CanvasHandCard.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CanvasHandCard : MonoBehaviour
+{
+    public HorizontalLayoutGroup layoutElement;
+
+    private void OnMouseEnter()
+    {
+        //layoutElement.spacing = 0.2f;
+    }
+}
