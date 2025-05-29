bfdd628e216bb2b28c9d101579e2e33bedaee866
MZdrenka <mar.zdr99@gmail.com>
2025-05-29 19:12:43 +0200

    [update]

--- Assets/[Script]/Card.cs
+++ Assets/[Script]/Card.cs
@@ -9,12 +9,19 @@ public class Card : MonoBehaviour
     public TMP_Text text;
     public LayoutElement layoutElement;
     public Collider Collider;
+    public GameObject CardElement;
 
     public GameObject Buttons;
 
     private void OnMouseEnter()
     {
         Debug.Log("OnMouseEnter");
+        CardElement.transform.localPosition = new Vector3(0f, 0.3f, 0f); 
+    }
+
+    private void OnMouseExit()
+    {
+        CardElement.transform.localPosition = new Vector3(0f, 0f, 0f);
     }
 
     private void OnMouseDown()
@@ -23,14 +30,13 @@ public class Card : MonoBehaviour
         {
             GlobalInformation.CardChoosen.layoutElement.ignoreLayout = false;
             GlobalInformation.CardChoosen.Collider.enabled = true;
-            GlobalInformation.CardChoosen.transform.localPosition = new Vector3(0f, 0f, 0f);
+            GlobalInformation.CardChoosen.transform.localPosition = new Vector3(0f, 0f, -0.08f);
             GlobalInformation.CardChoosen.transform.localScale = new Vector3(1f, 1f, 1f);
             GlobalInformation.CardChoosen.Buttons.SetActive(false);
         }
         GlobalInformation.CardChoosen = this;
         layoutElement.ignoreLayout = true;
         transform.localPosition = new Vector3(0, 3f, 0f);
-        new Vector3(1f, 1f, 2f);
         transform.localScale = new Vector3(2f, 2f, 2f);
         Collider.enabled = false;
         Buttons.SetActive(true);
